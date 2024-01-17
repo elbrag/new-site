326e41da5b229325e3594876312633e60950c63b
Ellen <ellen.brage@welcom.se>
2024-01-17 18:30:51 +0100

    Create game pages and ensure styled components work as intended.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-        config.module.rules.push({
-          test: /\.svg$/i,
-          issuer: /\.[jt]sx?$/,
-          use: ['@svgr/webpack'],
-        })
+	reactStrictMode: true,
+	experimental: {
+		styledComponents: true,
+	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ["@svgr/webpack"],
+		});
 
-        return config
-      },
-}
+		return config;
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/memory.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Puzzle: React.FC = () => {
+	return <div></div>;
+};
+
+export default Puzzle;
