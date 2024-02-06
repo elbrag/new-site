f7d1a62d3ffa11aceef8da08f9cc4878b671c255
Ellen <ellen.brage@welcom.se>
2024-02-06 15:44:54 +0100

    Move user id setting to useFirebase hook.

--- hooks/useFirebase.ts
+++ hooks/useFirebase.ts
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { Database, getDatabase } from "firebase/database";
+import { v4 as uuidv4 } from "uuid";
 
 let firebaseApp: FirebaseApp | undefined;
 let firebaseDatabase: Database;
 
 const useFirebase = () => {
+	const [userId, setUserId] = useState<string | null>(null);
+
+	// Init Firebase
 	useEffect(() => {
 		if (!firebaseApp) {
 			const firebaseConfig = {
@@ -17,7 +21,19 @@ const useFirebase = () => {
 		}
 	}, []);
 
-	return { database: firebaseDatabase };
+	// Set user id to use when making calls to Firebase
+	useEffect(() => {
+		const storedUserId = localStorage.getItem("userId");
+		if (storedUserId) {
+			setUserId(storedUserId);
+		} else {
+			const newUserId = uuidv4();
+			localStorage.setItem("userId", newUserId);
+			setUserId(newUserId);
+		}
+	}, []);
+
+	return { database: firebaseDatabase, userId };
 };
 
 export default useFirebase;
--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,24 +1,10 @@
 import { useEffect, useState } from "react";
 import { ref, set, get } from "firebase/database";
-import { v4 as uuidv4 } from "uuid";
 import useFirebase from "./useFirebase";
 
 const useUser = () => {
-	const [userId, setUserId] = useState<string | null>(null);
 	const [username, setUsername] = useState<string | null>(null);
-	const { database } = useFirebase();
-
-	// Set user id to use when making calls to Firebase
-	useEffect(() => {
-		const storedUserId = localStorage.getItem("userId");
-		if (storedUserId) {
-			setUserId(storedUserId);
-		} else {
-			const newUserId = uuidv4();
-			localStorage.setItem("userId", newUserId);
-			setUserId(newUserId);
-		}
-	}, []);
+	const { database, userId } = useFirebase();
 
 	// Get username
 	useEffect(() => {
