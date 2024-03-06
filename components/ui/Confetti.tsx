0337dcbb07cad36b89b29a928d116f0c70e9610c
Ellen <ellen.brage@welcom.se>
2024-03-06 21:59:26 +0100

    Change in confetti, add some test rounds.

--- components/ui/Confetti.tsx
+++ components/ui/Confetti.tsx
@@ -1,4 +1,6 @@
+import { motion } from "framer-motion";
 import React from "react";
+import shuffle from "lodash/shuffle";
 
 interface ConfettiProps {}
 
@@ -13,14 +15,24 @@ const Confetti: React.FC<ConfettiProps> = ({}) => {
 		"purple",
 	];
 
+	const getConfettiArray = () => {
+		const array = [];
+		for (const confetti of confettiColors) {
+			for (let i = 0; i < 20; i++) {
+				array.push(confetti);
+			}
+		}
+		return shuffle(array);
+	};
+
 	return (
-		<div className="confetti hidden fixed w-screen h-screen flex justify-center items-center">
-			<div className="flex">
-				{Array.from(confettiColors).map((color, i) => (
-					<span
+		<div className="confetti hidden w-screen h-screen flex justify-center items-center">
+			<div className="flex items-center justify-center">
+				{getConfettiArray().map((color, i) => (
+					<motion.span
 						key={`color-${color}-${i}`}
-						className={`inline-block w-4 h-4 bg-confetti-${color}`}
-					></span>
+						className={`inline w-4 h-4 bg-confetti-${color}`}
+					></motion.span>
 				))}
 			</div>
 		</div>
