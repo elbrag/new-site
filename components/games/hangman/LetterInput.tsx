effcc7f6000faf4a7ee68a2fa3f8eedf5948f5b3
Ellen <ellen.brage@welcom.se>
2024-09-02 08:14:10 +0200

    Add listener to enter + auto focus to input in letter input.

--- components/games/hangman/LetterInput.tsx
+++ components/games/hangman/LetterInput.tsx
@@ -1,5 +1,11 @@
 import Button from "@/components/ui/Button";
-import React, { ChangeEvent, useState } from "react";
+import React, {
+	ChangeEvent,
+	KeyboardEvent,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
 
 interface LetterInputProps {
 	onClick: (inputValue: string) => void;
@@ -7,7 +13,15 @@ interface LetterInputProps {
 
 const LetterInput: React.FC<LetterInputProps> = ({ onClick }) => {
 	const [inputValue, setInputValue] = useState("");
+	const input = useRef<HTMLInputElement | null>(null);
 
+	useEffect(() => {
+		focusInput();
+	}, []);
+
+	/**
+	 * Handle input change
+	 */
 	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const value = e.currentTarget.value;
 
@@ -16,22 +30,44 @@ const LetterInput: React.FC<LetterInputProps> = ({ onClick }) => {
 		}
 	};
 
+	/**
+	 * Handle keydown
+	 */
+	const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter" || e.keyCode === 13) {
+			onSubmit();
+		}
+	};
+
+	/**
+	 * On Submit
+	 */
+	const onSubmit = () => {
+		onClick(inputValue);
+		setInputValue("");
+		focusInput();
+	};
+
+	/**
+	 * Focus letter input
+	 */
+	const focusInput = () => {
+		input.current?.focus();
+	};
+
 	return (
 		<div className="letter-input flex items-center gap-4">
 			<input
+				ref={input}
+				id="letter"
 				type="text"
 				className="text-2xl text-center p-2 focus:outline-none active:outline-none w-20 uppercase border-military border-2 bg-paper"
 				maxLength={1}
 				onChange={(e) => handleInputChange(e)}
+				onKeyDown={handleKeyDown}
 				value={inputValue}
 			/>
-			<Button
-				label="Guess"
-				onClick={() => {
-					onClick(inputValue);
-					setInputValue("");
-				}}
-			/>
+			<Button label="Guess" onClick={onSubmit} />
 		</div>
 	);
 };
