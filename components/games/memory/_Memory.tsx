d0cf3669cb423427614f764575098d3f18fb066c
Ellen <ellen.brage@welcom.se>
2024-09-24 19:46:49 +0200

    Add transition to memory cards, set number of rounds at all times to avoid inheriting them from other games, make it impossible to click a card twice and win.

--- components/games/memory/_Memory.tsx
+++ components/games/memory/_Memory.tsx
@@ -170,7 +170,7 @@ const Memory: React.FC<MemoryProps> = ({ gameData }) => {
 	 */
 	const onCardClick = async (index: number) => {
 		// Already found: Show card data in modal
-		if (getCardData(index)) {
+		if (getFoundCardDataBySlotindex(index)) {
 			setModalCardContent(getCardData(index));
 		}
 		// Clicked card already flipped
@@ -192,9 +192,11 @@ const Memory: React.FC<MemoryProps> = ({ gameData }) => {
 	};
 
 	/**
-	 * Get card data by index
+	 * Get found card data by slot index
+	 *
+	 * Returns card data if it has been found
 	 */
-	const getCardData = (index: number) => {
+	const getFoundCardDataBySlotindex = (index: number) => {
 		const dataFound = foundCardData.length
 			? foundCardData.find((data) =>
 					data.images.some(
@@ -202,6 +204,14 @@ const Memory: React.FC<MemoryProps> = ({ gameData }) => {
 					)
 			  )
 			: false;
+		if (dataFound) return dataFound;
+	};
+
+	/**
+	 * Get card data by index
+	 */
+	const getCardData = (index: number) => {
+		const dataFound = getFoundCardDataBySlotindex(index);
 		if (dataFound) return dataFound;
 
 		return activeCard1 === index
@@ -228,117 +238,121 @@ const Memory: React.FC<MemoryProps> = ({ gameData }) => {
 	return (
 		<div>
 			{readyToRenderGame && (
-				<AnimatePresence mode="wait">
-					<motion.div
-						className="md:px-6 lg:px-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-center"
-						initial={{
-							opacity: 0,
-						}}
-						animate={{
-							opacity: 1,
-						}}
-						exit={{
-							opacity: 0,
-						}}
-						transition={{
-							duration: 0.5,
-						}}
-					>
-						{/* Cards */}
-						{Array.from({
-							length: cardCount,
-						}).map((_, index) => (
-							<MemoryCard
-								key={`card-${index}`}
-								cardData={getCardData(index)}
-								onClick={() => onCardClick(index)}
-								className={index === activeCard2 ? "z-1" : ""}
+				<>
+					{/* Cards */}
+					{cardCount && (
+						<AnimatePresence>
+							<motion.div
+								className="md:px-6 lg:px-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 justify-center"
+								initial={{
+									opacity: 0,
+								}}
+								animate={{
+									opacity: 1,
+								}}
+								exit={{
+									opacity: 0,
+								}}
+								transition={{
+									duration: 0.5,
+								}}
 							>
-								<AnimatePresence>
-									{infoMessage && index === activeCard2 && (
-										<div className="w-full absolute bottom-1/4 left-0 md:left-[unset] md:-right-1/2 z-1">
-											<InfoMessage text={infoMessage} />
-										</div>
-									)}
-								</AnimatePresence>
-							</MemoryCard>
-						))}
-						<AnimatePresence>
-							{successMessage && <SuccessScreen text={successMessage} />}
+								{Array.from({
+									length: cardCount,
+								}).map((_, index) => (
+									<MemoryCard
+										key={`card-${index}`}
+										cardData={getCardData(index)}
+										onClick={() => onCardClick(index)}
+										className={index === activeCard2 ? "z-1" : ""}
+									>
+										<AnimatePresence>
+											{infoMessage && index === activeCard2 && (
+												<div className="w-full absolute bottom-1/4 left-0 md:left-[unset] md:-right-1/2 z-1">
+													<InfoMessage text={infoMessage} />
+												</div>
+											)}
+										</AnimatePresence>
+									</MemoryCard>
+								))}
+							</motion.div>
 						</AnimatePresence>
-						<AnimatePresence>{/* <Confetti /> */}</AnimatePresence>
-						{/* Modal */}
-						<AnimatePresence>
-							{modalCardContent && (
-								<Modal
-									onClose={onModalClose}
-									className="overflow-hidden max-w-184"
-									motionKey="memory-modal"
-								>
-									<h2 className="text-xl lg:text-2xl mb-10 uppercase">
-										It&apos;s a match!
-									</h2>
-									<div className="mb-6 flex flex-col justify-center">
-										<h3 className="mb-8 text-center">
-											{modalCardContent.description}
-										</h3>
-										<p>
-											{modalCardContent.subtitle && modalCardContent.subtitle}
-										</p>
-										<ul
-											className={`relative mx-auto h-44 md:h-52 grid place-items-center grid-cols-${modalCardContent?.images.length}`}
-											style={{
-												gridTemplateColumns: `repeat(${modalCardContent?.images.length}, 11rem)`,
-											}}
-										>
-											<AnimatePresence>
-												{modalCardContent?.images.map((image, i) => {
-													const rotation = rotationValues[i];
-													return (
-														<li
-															className={`w-44 h-inherit flex justify-center ${
-																i === 0 ? "z-1" : "absolute left-0 z-0"
-															}`}
-															key={`found-img-li-`}
+					)}
+					<AnimatePresence>
+						{successMessage && <SuccessScreen text={successMessage} />}
+					</AnimatePresence>
+					<AnimatePresence>{/* <Confetti /> */}</AnimatePresence>
+					{/* Modal */}
+					<AnimatePresence>
+						{modalCardContent && (
+							<Modal
+								onClose={onModalClose}
+								className="overflow-hidden max-w-184"
+								motionKey="memory-modal"
+							>
+								<h2 className="text-xl lg:text-2xl mb-10 uppercase">
+									It&apos;s a match!
+								</h2>
+								<div className="mb-6 flex flex-col justify-center">
+									<h3 className="mb-8 text-center">
+										{modalCardContent.description}
+									</h3>
+									<p>
+										{modalCardContent.subtitle && modalCardContent.subtitle}
+									</p>
+									<ul
+										className={`relative mx-auto h-44 md:h-52 grid place-items-center grid-cols-${modalCardContent?.images.length}`}
+										style={{
+											gridTemplateColumns: `repeat(${modalCardContent?.images.length}, 11rem)`,
+										}}
+									>
+										<AnimatePresence>
+											{modalCardContent?.images.map((image, i) => {
+												const rotation = rotationValues[i];
+												return (
+													<li
+														className={`w-44 h-inherit flex justify-center ${
+															i === 0 ? "z-1" : "absolute left-0 z-0"
+														}`}
+														key={`found-img-li-${i}`}
+													>
+														<motion.div
+															key={`found-img-container-${i}`}
+															className={`w-32 md:w-36 h-inherit rounded-md overflow-hidden `}
+															initial={{
+																rotateZ: 0,
+																x: 0,
+																transformOrigin: "50% 50%",
+															}}
+															animate={{
+																rotateZ: rotation,
+																x: `calc(11rem * ${i})`,
+																transformOrigin: "50% 50%",
+															}}
+															transition={{
+																duration: 0.6,
+																delay: i * 0.5 * (timeoutTime / 1000),
+																ease: [0.42, 0, 0.58, 1],
+															}}
 														>
-															<motion.div
-																key={`found-img-container-`}
-																className={`w-32 md:w-36 h-inherit rounded-md overflow-hidden `}
-																initial={{
-																	rotateZ: 0,
-																	x: 0,
-																	transformOrigin: "50% 50%",
-																}}
-																animate={{
-																	rotateZ: rotation,
-																	x: `calc(11rem * )`,
-																	transformOrigin: "50% 50%",
-																}}
-																transition={{
-																	duration: 0.6,
-																	delay: i * 0.5 * (timeoutTime / 1000),
-																	ease: [0.42, 0, 0.58, 1],
-																}}
-															>
-																<Image
-																	className="min-h-full object-cover"
-																	src={`/static/images/memory/${image.url}.jpg`}
-																	alt=""
-																	width={500}
-																	height={500}
-																/>
-															</motion.div>
-														</li>
-													);
-												})}
-											</AnimatePresence>
-										</ul>
-									</div>
-								</Modal>
-							)}
-						</AnimatePresence>
-					</motion.div>
-				</AnimatePresence>
+															<Image
+																className="min-h-full object-cover"
+																src={`/static/images/memory/${image.url}.jpg`}
+																alt=""
+																width={500}
+																height={500}
+															/>
+														</motion.div>
+													</li>
+												);
+											})}
+										</AnimatePresence>
+									</ul>
+								</div>
+							</Modal>
+						)}
+					</AnimatePresence>
+				</>
 			)}
 		</div>
 	);
