3470343b0903120123be0a75b3a20320817bea16
Ellen <ellen.brage@welcom.se>
2025-02-17 18:01:08 +0100

    Put back button on result page, memory

--- components/games/memory/FoundCards.tsx
+++ components/games/memory/FoundCards.tsx
@@ -6,6 +6,7 @@ import { wellDoneHeading } from "@/lib/helpers/messages";
 import { motion } from "framer-motion";
 import { StyledCardGrid } from "./_Memory";
 import { makeMemoryImgUrl, preloadImage } from "@/lib/helpers/images";
+import BackButton from "@/components/ui/BackButton";
 
 interface FoundCardsProps {
 	cardData: MemoryRoundProps[];
@@ -22,7 +23,6 @@ const FoundCards: React.FC<FoundCardsProps> = ({ cardData }) => {
 
 	return (
 		<motion.div
-			className="text-center"
 			style={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			transition={{
@@ -30,44 +30,49 @@ const FoundCards: React.FC<FoundCardsProps> = ({ cardData }) => {
 				ease: "easeInOut",
 			}}
 		>
-			<h2 className="font-alegreya lg:text-lg mb-12 lg:mb-16">
-				{wellDoneHeading}
-			</h2>
-			{cardData.map((data, i) => (
-				<div key={`card-list-${i}`}>
-					<div className="mb-8">
-						<h3 className="text-base">{data.description}</h3>
-						{data.subtitle && (
-							<p className="font-alegreya mt-2">{data.subtitle}</p>
-						)}
-					</div>
-					<div className="flex justify-center mb-20">
-						<StyledCardGrid
-							$numberOfCards={data.images.length}
-							className={`relative mx-auto h-fit grid place-items-center gap-2 sm:gap-0`}
-						>
-							{data.images.map((image, ii) => {
-								const rotation = rotationValues[(ii + 1) * i];
+			<div className="mb-8 md:mb-4">
+				<BackButton />
+			</div>
+			<div className="text-center">
+				<h2 className="font-alegreya lg:text-lg mb-12 lg:mb-16">
+					{wellDoneHeading}
+				</h2>
+				{cardData.map((data, i) => (
+					<div key={`card-list-${i}`}>
+						<div className="mb-8">
+							<h3 className="text-base">{data.description}</h3>
+							{data.subtitle && (
+								<p className="font-alegreya mt-2">{data.subtitle}</p>
+							)}
+						</div>
+						<div className="flex justify-center mb-20">
+							<StyledCardGrid
+								$numberOfCards={data.images.length}
+								className={`relative mx-auto h-fit grid place-items-center gap-2 sm:gap-0`}
+							>
+								{data.images.map((image, ii) => {
+									const rotation = rotationValues[(ii + 1) * i];
 
-								return (
-									<li
-										key={`card-${ii}`}
-										className={`w-36 md:w-44 h-inherit flex justify-center ${
-											ii === 0 ? "z-1" : "sm:absolute left-0 z-0"
-										}`}
-									>
-										<MemoryRevealedImage
-											imgUrl={image.url}
-											index={ii}
-											rotation={rotation}
-										/>
-									</li>
-								);
-							})}
-						</StyledCardGrid>
+									return (
+										<li
+											key={`card-${ii}`}
+											className={`w-36 md:w-44 h-inherit flex justify-center ${
+												ii === 0 ? "z-1" : "sm:absolute left-0 z-0"
+											}`}
+										>
+											<MemoryRevealedImage
+												imgUrl={image.url}
+												index={ii}
+												rotation={rotation}
+											/>
+										</li>
+									);
+								})}
+							</StyledCardGrid>
+						</div>
 					</div>
-				</div>
-			))}
+				))}
+			</div>
 		</motion.div>
 	);
 };
