2846e7990812c5331b78fe36dd5bd4e444d6b49a
Ellen Brage <ellen.brage@vgregion.se>
2025-12-05 08:44:17 +0100

    Upgrade react, next, nodemailer and intersect-obs

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	compiler: {
-		styledComponents: true,
-	},
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			use: ["@svgr/webpack"],
-		});
-
-		return config;
-	},
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
